97f07d186cf527f15d31c0884e44fa2ba7cc0046
„Szymon <szymonk@onet.pl>
2021-01-11 09:21:10 +0100

    Wykonano zadanie 3.3.1 –tekst statyczny

--- Zadanie-3-3/src/main.cpp
+++ Zadanie-3-3/src/main.cpp
@@ -1,7 +1,13 @@
 #include <Arduino.h>
-
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 void setup() {  
+lcd.begin(16, 2);
+lcd.print("Kwiecien");
 }
 
 void loop() {  
+    lcd.setCursor(0, 1);
+    lcd.print(millis() / 1000);
+    delay(500);
 }
