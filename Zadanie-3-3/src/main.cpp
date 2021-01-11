5d624b961e1642a58d4a10c2091fbd00acddb8fd
„Szymon <szymonk@onet.pl>
2021-01-11 15:15:45 +0100

    Wykonano zadanie dodatkowe 3.3.4 –stoper

--- Zadanie-3-3/src/main.cpp
+++ Zadanie-3-3/src/main.cpp
@@ -1,26 +1,60 @@
 #include <Arduino.h>
 #include <LiquidCrystal.h>
-#include <stdio.h>
 
+int przyciskstan = 0;
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-char rxBuff[32];
-int ValueA = 0, ValueB = 0;
-char operation[16];
+
+int godziny1, minuty1, sekundy1, godziny2, minuty2, sekundy2;
+char czas1[16];
+char czas2[16];
 
 void setup() {
-    lcd.begin(16, 2);
-    lcd.print("Szymon Kwiecień");
-    Serial.begin(9600);
-    Serial.setTimeout(3000);
-    delay(2000);
+   lcd.begin(16, 2);
+   pinMode(7, INPUT);
 }
 
 void loop() {
-   
-        Serial.readyBytesUntil('\r', rxBuff, sizeof(rxBuff) - 1);
-        lcd.setCursor(0, 0);
-        lcd.print(rxBuff);
-        lcd.setCursor(0, 1);
-        lcd.print('=');
-    
+
+  przyciskstan = digitalRead(7);
+  if (przyciskstan == HIGH) {
+  sekundy2++;    
+    if (sekundy2 == 60)
+  {
+    sekundy2 = 0;
+    minuty2 ++;
+     if (minuty2 == 60)
+  {
+    minuty2 = 0;
+    godziny2 ++;
+
+  }
+  }
+  }
+
+  sekundy1++;
+  
+  if (sekundy1 == 60)
+  {
+    sekundy1 = 0;
+    minuty1 ++;
+     if (minuty1 == 60)
+  {
+    minuty1 = 0;
+    godziny1 ++;
+
+  }
+  }
+  lcd.setCursor(0, 0); 
+    lcd.print(godziny2);
+  lcd.print(":");
+  lcd.print(minuty2);
+  lcd.print(":");
+  lcd.print(sekundy2);
+  lcd.setCursor(0, 1); 
+  lcd.print(godziny1);
+  lcd.print(":");
+  lcd.print(minuty1);
+  lcd.print(":");
+  lcd.print(sekundy1);
+  delay(10);   // sekunda to 10ms
 }
